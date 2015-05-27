491b79047ee40bb806ad3586321a66cf588db794
Amy Boyd <amy@amyboyd.co.uk>
2015-05-27 23:08:36 +0100

    Remove old and now redundant order status, NOT_PLACED.

--- tests/unit/factories/OrdersFactory.spec.js
+++ tests/unit/factories/OrdersFactory.spec.js
@@ -17,7 +17,7 @@ describe('OrdersFactory', function () {
         it('should resolve to the nearest placed order from an array of orders of varying dates and statuses', function() {
             $httpBackend.expectGET('http://api-base/orders/by-current-customer').respond({
                 orders: [
-                    {id: 1, statusText: 'not_placed', requestedDeliveryDate: '2020-02-01T12:00:00Z'},
+                    {id: 1, statusText: 'pending_vendor_approval', requestedDeliveryDate: '2020-02-04T12:00:00Z'},
                     {id: 2, statusText: 'pending_vendor_approval', requestedDeliveryDate: '2020-02-03T12:00:00Z'},
                     {id: 3, statusText: 'accepted', requestedDeliveryDate: '2020-02-02T12:00:00Z'},
                 ],
