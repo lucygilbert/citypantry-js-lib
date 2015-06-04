8db8f770f1f711bf6ff9bd9f0de8e7dc1e5981a7
Lucy Gilbert-Boyd <lucygilbert01@gmail.com>
2015-06-04 20:09:07 +0100

    Add review factory.

---
+++ src/factories/ReviewFactory.js
@@ -0,0 +1,5 @@
+angular.module('cpLib').factory('ReviewFactory', function(ApiService) {
+    return {
+        setReviewAsPublic: (isPublic, reviewId) => ApiService.put(`/reviews/${reviewId}/set-as-public`, {isPublic: isPublic}),
+    };
+});
