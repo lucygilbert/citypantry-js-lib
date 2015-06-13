d0b2fefe7a3281467e714eaf66022f0455930a72
Lucy Gilbert-Boyd <lucygilbert01@gmail.com>
2015-06-14 00:48:44 +0100

    Add get review by ID API.

--- src/factories/ReviewFactory.js
+++ src/factories/ReviewFactory.js
@@ -2,6 +2,8 @@ angular.module('cpLib').factory('ReviewFactory', function(ApiService) {
     return {
         setReviewAsPublic: (isPublic, reviewId) => ApiService.put(`/reviews/${reviewId}/set-as-public`, {isPublic: isPublic}),
 
-        getRecentReviews: () => ApiService.get(`/reviews/recent-by-current-vendor`)
+        getRecentReviews: () => ApiService.get(`/reviews/recent-by-current-vendor`),
+
+        getReview: (reviewId) => ApiService.get(`/reviews/${reviewId}`)
     };
 });
