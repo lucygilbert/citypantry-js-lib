a14be99b0f2191b12c0ebd5d1a651f7ed8c016ae
Lucy Gilbert-Boyd <lucygilbert01@gmail.com>
2015-07-02 16:12:58 +0100

    Modify the PackageDisposition texts for the changes to Must Not Have.

--- src/filters/getPackageDispositionTextFilter.js
+++ src/filters/getPackageDispositionTextFilter.js
@@ -4,9 +4,12 @@ angular.module('cpLib').filter('getPackageDispositionText', function() {
             case 'must_have':
             case 1:
                 return 'Must have';
-            case 'must_not_have':
+            case 'must_not_have_per_staff':
             case 2:
-                return 'Must not have';
+                return 'Must not have (per staff)';
+            case 'must_not_have_per_customer':
+            case 3:
+                return 'Must not have (per customer)';
             default:
                 throw 'Unexpected disposition: ' + disposition;
         }
