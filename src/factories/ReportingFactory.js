47370a037e75e26f7efce816c8b2ad8614613929
Amy Boyd <amy@amyboyd.co.uk>
2015-07-24 15:36:44 +0100

    Add ReportingFactory.runPresetDatabaseQuery().

--- src/factories/ReportingFactory.js
+++ src/factories/ReportingFactory.js
@@ -10,5 +10,16 @@ angular.module('cpLib').factory('ReportingFactory', function(ApiService) {
             };
             return ApiService.post(`/reporting/database-query`, {query: query}, config);
         },
+
+        runPresetDatabaseQuery(query, args) {
+            const config = {
+                // Angular will try to parse responses as JSON by default. The response from this
+                // API can be in any format, so turn off the JSON parsing,
+                transformResponse: [function(responseContent) {
+                    return responseContent;
+                }]
+            };
+            return ApiService.post(`/reporting/database-query/preset/${query}`, {args: args}, config);
+        }
     };
 });
