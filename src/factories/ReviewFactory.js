93c73c619767461193367b90204c396cd975c34f
Lucy Gilbert-Boyd <lucygilbert01@gmail.com>
2015-08-02 14:02:03 +0100

    Modify RecentReviews in ReviewFactory to take a day limit.

--- src/factories/ReviewFactory.js
+++ src/factories/ReviewFactory.js
@@ -2,7 +2,7 @@ angular.module('cpLib').factory('ReviewFactory', function(ApiService) {
     return {
         setReviewAsPublic: (isPublic, reviewId) => ApiService.put(`/reviews/${reviewId}/set-as-public`, {isPublic: isPublic}),
 
-        getRecentReviews: () => ApiService.get(`/reviews/recent-by-current-vendor`),
+        getRecentReviews: (dayLimit) => ApiService.get(`/reviews/recent-by-current-vendor?days=${dayLimit}`),
 
         getReview: (reviewId) => ApiService.get(`/reviews/${reviewId}`),
 
